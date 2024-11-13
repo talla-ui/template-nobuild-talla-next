b62b1e62291edd8b73767b85122412952d814b49
Jelmer Cormont <jelmer@cormont.work>
2024-11-14 00:06:34 +0800

    Chore: npm upgrade

--- site/MainActivity.js
+++ site/MainActivity.js
@@ -1,7 +1,9 @@
 import { $activity, Activity, ui } from "./lib/talla-web.es2022.esm.min.js";
 
 const view = ui.cell(
-  ui.label($activity.bind("count"), { fontSize: 40, tabularNums: true }),
+  ui.label($activity.bind("count"), {
+    style: { fontSize: 40, tabularNums: true },
+  }),
   ui.button({
     icon: ui.icon.PLUS,
     accessibleLabel: "Increment counter",

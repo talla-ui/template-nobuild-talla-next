b491d2f297eb22b9198226067aa84e0972e2d534
Jelmer Cormont <jelmer@cormont.work>
2025-01-31 23:54:37 +0800

    Chore: npm upgrade

--- site/MainActivity.js
+++ site/MainActivity.js
@@ -1,15 +1,18 @@
 import { $activity, Activity, ui } from "./lib/talla-web.es2022.esm.min.js";
 
 const view = ui.cell(
-  ui.label($activity("count"), {
-    style: { fontSize: 40, tabularNums: true },
-  }),
-  ui.button({
-    icon: ui.icon.PLUS,
-    accessibleLabel: "Increment counter",
-    onClick: "CountUp",
-    style: ui.style.BUTTON_SUCCESS,
-  })
+  ui.column(
+    { gravity: "center", distribute: "center" },
+    ui.label($activity("count"), {
+      style: { fontSize: 40, tabularNums: true },
+    }),
+    ui.button({
+      icon: ui.icon.PLUS,
+      accessibleLabel: "Increment counter",
+      onClick: "CountUp",
+      style: ui.style.BUTTON_SUCCESS,
+    })
+  )
 );
 
 export class MainActivity extends Activity {
